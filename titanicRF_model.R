84990dcbd6234b02ab0e531c05a65e5f1f656519
Jason West <jason.west@racelabglobal.com>
2021-07-09 12:39:24 +1000

    Tree spec update 1

--- titanicRF_model.R
+++ titanicRF_model.R
@@ -7,6 +7,17 @@ mydata <- read.csv("G:/My Drive/Research Clients/RaceLab/Data/Testing/titanic_mu
 head(mydata)
 summary(mydata)
 
+# survival	Survival	0 = No, 1 = Yes
+# pclass	Ticket class	1 = 1st, 2 = 2nd, 3 = 3rd
+# sex	Sex	
+# Age	Age in years	
+# sibsp	# of siblings / spouses aboard the Titanic	
+# parch	# of parents / children aboard the Titanic	
+# ticket	Ticket number	
+# fare	Passenger fare	
+# cabin	Cabin number	
+# embarked	Port of Embarkation	C = Cherbourg, Q = Queenstown, S = Southampton
+
 sapply(mydata, sd)
 
 str(mydata)
@@ -61,3 +72,13 @@ predicted.classes <- ifelse(probabilities > 0.5, 1, 0)
 mean(predicted.classes == mydata$Survived)
 
 # Random Forest
+library(tidymodels)
+# specify model settings
+decision_tree() %>% set_engine("rpart") %>% set_mode("classification")
+
+# the specification is a skeleton and needs to be trained with data
+tree_spec <- decision_tree() %>% 
+  set_engine("rpart") %>% 
+  set_mode("classification")
+
+tree_spec %>% fit(formula = Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=mydata)
